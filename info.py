d409fae2dcc2eeab1d71af272f01e1a678953d4c
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-10-14 12:28:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://VAISHNAV:VAISHNAV@cluster0.sn8ij4b.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://VAISHNAV:VAISHNAV@cluster0.sn8ij4b.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "VAISHNAV")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
